2c6f21e42eb11b8652a8dd5b0f95c3b3320d2067
Vashchenia Leonid <dcs.lvv.87@gmail.com>
2022-09-22 10:58:01 +0300

    corect-06

--- js/task-06.js
+++ js/task-06.js
@@ -5,6 +5,7 @@ console.log(checkInput);
 
 inputRef.addEventListener("blur", (event) => {
   if (event.currentTarget.value.length !== checkInput) {
+    inputRef.classList.remove("valid");
     inputRef.classList.add("invalid");
   } else {
     inputRef.classList.add("valid");
